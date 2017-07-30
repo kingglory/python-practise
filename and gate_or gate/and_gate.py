313503a0f58916259218a43c7d801f7f230cb0c8
Wangwensong <312775704@qq.com>
2017-07-30 11:30:21 +0800

    Update and_gate.py
    clean up

--- and gate_or gate/and_gate.py
+++ and gate_or gate/and_gate.py
@@ -1,9 +1,5 @@
 #coding=utf-8
 
-
-
-
-
 input1=[0, 0, 1, 1]
 input2=[0, 1, 0, 1]
 h=[]
@@ -19,9 +15,7 @@ h=[]
 for x in range(0,4):                    #把4改成3结果会变成1,1,1，1
     h.append((not(input1[x] and input2[x])))      #此处不添加int（）输出结果为True，True，True，False
                                          #加上int（）之后变为了1，1,1,0
-
 print "output for NAND:",h
-
 
 inp1=[False,False,True,True]
 inp2 = [False,True,False,True]
